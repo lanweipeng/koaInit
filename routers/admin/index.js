c1d32d07ad2111a24e06a6ca303eb5fc88e21b12
lwp <1462191066@qq.com>
2019-06-25 01:51:15 +0800

    1.1

--- routers/admin/index.js
+++ routers/admin/index.js
@@ -1,6 +1,16 @@
 const Router=require('koa-router');
+const fs=require('await-fs')
+const path=require('path')
 let router = new Router();
 router.get('/login',async ctx=>{
     await ctx.render('admin/login',{})
+})
+router.post('/login',async ctx=>{
+    let {uname,upassword}=ctx.request.fields;
+    let admins=JSON.parse((await fs.readFile(
+        path.resolve(__dirname,'../../admin.json')
+    )).toString());
+    console.log(admins)
+    ctx.body=admins;
 })
 module.exports=router.routes();
